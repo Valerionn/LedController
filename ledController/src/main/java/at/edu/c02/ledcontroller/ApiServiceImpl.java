25a007b97fab4102ebabe2435469c275bdd9c4a1
chris <christopher.schmidt89@gmx.at>
2023-01-21 09:56:38 +0100

    Implementierung der getLight(id) Methode, um den Status einer spezifischen ID abzufragen. Implementierung  und der initialize() Methode, um mehrfach verwendeten Code der klasse auszulagern.
    closes#1

--- ledController/src/main/java/at/edu/c02/ledcontroller/ApiServiceImpl.java
+++ ledController/src/main/java/at/edu/c02/ledcontroller/ApiServiceImpl.java
@@ -25,26 +25,44 @@ public class ApiServiceImpl implements ApiService {
     @Override
     public JSONObject getLights() throws IOException
     {
-        // Connect to the server
-        URL url = new URL("https://balanced-civet-91.hasura.app/api/rest/getLights");
+
+        String command = "https://balanced-civet-91.hasura.app/api/rest/getLights";
+        String method = "GET";
+        JSONObject result = initialize(command,method);
+        return result;
+
+    }
+
+    @Override
+    public JSONObject getLight(int id) throws IOException {
+
+        String command = "https://balanced-civet-91.hasura.app/api/rest/lights/:"+ id;
+        String method = "GET";
+        JSONObject result = initialize(command,method);
+        return result;
+
+    }
+
+    public JSONObject initialize(String command, String method) throws  IOException{
+
+        URL url = new URL(command);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        // and send a GET request
-        connection.setRequestMethod("GET");
-        connection.setRequestProperty("X-Hasura-Group-ID", "Todo");
-        // Read the response code
+
+        connection.setRequestMethod(method);
+        connection.setRequestProperty("H", "5f26cca3877ad");
+
         int responseCode = connection.getResponseCode();
         if(responseCode != HttpURLConnection.HTTP_OK) {
             // Something went wrong with the request
             throw new IOException("Error: getLights request failed with response code " + responseCode);
         }
 
-        // The request was successful, read the response
         BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        // Save the response in this StringBuilder
+
         StringBuilder sb = new StringBuilder();
 
         int character;
-        // Read the response, character by character. The response ends when we read -1.
+
         while((character = reader.read()) != -1) {
             sb.append((char) character);
         }
@@ -52,5 +70,6 @@ public class ApiServiceImpl implements ApiService {
         String jsonText = sb.toString();
         // Convert response into a json object
         return new JSONObject(jsonText);
+
     }
 }
