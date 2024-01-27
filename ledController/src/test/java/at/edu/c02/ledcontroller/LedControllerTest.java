106b14cc2bedf04e67183df1eb60ad816c39e02b
paul-c02 <00530734@edu.campus02.at>
2024-01-27 12:29:44 +0100

    real tests for getGroupLeds

--- ledController/src/test/java/at/edu/c02/ledcontroller/LedControllerTest.java
+++ ledController/src/test/java/at/edu/c02/ledcontroller/LedControllerTest.java
@@ -19,11 +19,11 @@ public class LedControllerTest {
      * This test is just here to check if tests get executed. Feel free to delete it when adding your own tests.
      * Take a look at the stack calculator tests again if you are unsure where to start.
      */
-    private JSONObject readJsonFile (String filename) throws FileNotFoundException {
+    private JSONObject readJsonFile (String filename) throws IOException {
         FileReader fr = new FileReader(filename);
         BufferedReader br = new BufferedReader(fr);
-
-        return new JSONObject("{\"lights\":[{\"id\":20,\"color\":\"#fff\",\"on\":false,\"groupByGroup\":{\"name\":\"C\"}}, {\"id\":21,\"color\":\"#fff\",\"on\":false,\"groupByGroup\":{\"name\":\"C\"}}, {\"id\":22,\"color\":\"#fff\",\"on\":false,\"groupByGroup\":{\"name\":\"C\"}}, {\"id\":23,\"color\":\"#fff\",\"on\":false,\"groupByGroup\":{\"name\":\"C\"}}, {\"id\":24,\"color\":\"#fff\",\"on\":false,\"groupByGroup\":{\"name\":\"C\"}}, {\"id\":25,\"color\":\"#fff\",\"on\":false,\"groupByGroup\":{\"name\":\"C\"}}, {\"id\":26,\"color\":\"#fff\",\"on\":false,\"groupByGroup\":{\"name\":\"C\"}}, {\"id\":27,\"color\":\"#fff\",\"on\":false,\"groupByGroup\":{\"name\":\"C\"}}]}\n");
+        return new JSONObject(br.readLine());
+        //return new JSONObject("{\"lights\":[{\"id\":20,\"color\":\"#fff\",\"on\":false,\"groupByGroup\":{\"name\":\"C\"}}, {\"id\":21,\"color\":\"#fff\",\"on\":false,\"groupByGroup\":{\"name\":\"C\"}}, {\"id\":22,\"color\":\"#fff\",\"on\":false,\"groupByGroup\":{\"name\":\"C\"}}, {\"id\":23,\"color\":\"#fff\",\"on\":false,\"groupByGroup\":{\"name\":\"C\"}}, {\"id\":24,\"color\":\"#fff\",\"on\":false,\"groupByGroup\":{\"name\":\"C\"}}, {\"id\":25,\"color\":\"#fff\",\"on\":false,\"groupByGroup\":{\"name\":\"C\"}}, {\"id\":26,\"color\":\"#fff\",\"on\":false,\"groupByGroup\":{\"name\":\"C\"}}, {\"id\":27,\"color\":\"#fff\",\"on\":false,\"groupByGroup\":{\"name\":\"C\"}}]}\n");
     }
     @Test
     public void dummyTest() {
@@ -51,6 +51,7 @@ public class LedControllerTest {
         JSONObject groupObject = readJsonFile("src/test/resources/groupByGroup.json");
         JSONArray group = groupObject.getJSONArray("lights");
        assertEquals(ledController.getGroupLeds().length(),group.length());
+       assertEquals(ledController.getGroupLeds().get(0).toString(),group.get(0).toString());
         // when(cal.perfom(Operation.add)).thenReturn(3.0);
 
     }
