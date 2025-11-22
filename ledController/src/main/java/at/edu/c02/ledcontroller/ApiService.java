5e4c9503e02549e68d89915bedb0a963e4a737b5
fgrad <sh55690@edu.campus02.at>
2025-11-22 09:44:38 +0100

    Fehlerkorrekturen Test für getGroupLeds() closes #9

--- ledController/src/main/java/at/edu/c02/ledcontroller/ApiService.java
+++ ledController/src/main/java/at/edu/c02/ledcontroller/ApiService.java
@@ -1,9 +1,11 @@
 package at.edu.c02.ledcontroller;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
 
 public interface ApiService {
     JSONObject getLights() throws IOException;
+    //JSONArray getGroupLeds(String groupName) throws IOException;
 }
