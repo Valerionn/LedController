2e31f945b759670211fac1a7102ccaebee96555d
Philip Pollheimer <52408170@edu.campus02.at>
2025-11-22 09:30:47 +0100

    APIService refactored

--- ledController/src/main/java/at/edu/c02/ledcontroller/ApiService.java
+++ ledController/src/main/java/at/edu/c02/ledcontroller/ApiService.java
@@ -6,5 +6,5 @@ import java.io.IOException;
 
 public interface ApiService {
     JSONObject getLights() throws IOException;
-    JSONObject getLight(int id) throws IOException;
+    JSONObject getLight(int ID) throws IOException;
 }
